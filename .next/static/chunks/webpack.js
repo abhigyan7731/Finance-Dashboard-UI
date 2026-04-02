906d64e94202b380294225303682e078bf85c5d8
ABHIGYAN KUMAR GUPTA <abhigyankumar268@gmail.com>
2026-04-03 02:11:26 +0530

    Update webpack runtime hash and add hot update files for improved build tracking

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a22f2cc40e67906b"; }
+/******/ 		__webpack_require__.h = function() { return "6f0822a112a207aa"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
